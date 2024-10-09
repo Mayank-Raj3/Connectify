395aec0efa4c048c276d9c07067a31b4432a1cfa
Mayank-Raj3 <guptamannu884@gmail.com>
2024-10-10 01:50:11 +0530

    get,next

--- app/app.js
+++ app/app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-  console.log(req);
-  res.send("Hello");
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  res.send({
+    firstname: "Mayank",
+    lastname: "Raj",
+  });
 });
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+    // res.send("Hellp");
+    next();
+  },
+  (req, res) => {
+    res.send("Hellp3");
+  }
+);
+
 app.listen(3000, () => {
   console.log("Running the server");
 });
